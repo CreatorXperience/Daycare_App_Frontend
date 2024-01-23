50ff231d7cabca37850d87d1d88e58af17a207d9
CreatorXperience <allyearmustobey@gmail.com>
2024-01-23 23:26:47 +0000

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      Welcome
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/colors.ts
@@ -0,0 +1,17 @@
+let colors = {
+    primary: {
+        blue: "#2988DD",
+        black: "#000000",
+        lemon:"#05B4BD",
+        green: "#3EBA4A",
+        gradientGreen: "linear-gradient(180deg, #29DD71 0%, #03A91D 100%)",
+        gradientBlue: "linear-gradient(180deg, #2988DD 0%, #035BA9 100%)",
+        yellow: "#FBD350",
+        grayishWhite: "#F8F8F8",
+        lineGray: "rgba(0, 0, 0, 0.05)",
+        shadow: "0px 2px 8px 0px rgba(0, 0, 0, 0.10)"
+
+    }
+}
+
+export {colors}
